34c49cd36ac62f866d882abe4ea54c101fb91ae5
jaayrad <zakubrad@gmail.com>
2023-03-20 17:58:52 +0100

    Collections

---
+++ src/Tutorial/Collections/ArrayList/Product.java
@@ -0,0 +1,24 @@
+package Tutorial.Collections.ArrayList;
+
+public class Product {
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product " +
+                " name='" + name + '\'';
+    }
+
+}
