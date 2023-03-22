98ba640195d0231edb9cfb1ddc930ac5ff97c157
jaayrad <zakubrad@gmail.com>
2023-03-22 23:54:22 +0100

    Threads

---
+++ src/Threads/MultiThreading/Main.java
@@ -0,0 +1,13 @@
+package Threads.MultiThreading;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i =0; i < 5; i++){
+            threading myThing = new threading(i);
+            myThing.start();
+        }
+
+
+
+    }
+}
