44e74bfb51242bcc10266e0d3dbc25d1dad485e4
jaayrad <zakubrad@gmail.com>
2023-03-23 01:53:32 +0100

    Threads & Runnable examples.

---
+++ src/Threads/FirstThread/Threads.java
@@ -0,0 +1,31 @@
+package Threads.FirstThread;
+
+import Threads.FirstThread.MyThread;
+
+public class Threads {
+    public static void main(String[] args) {
+//        Thread.currentThread().setName("Main Thread");
+////        System.out.println(Thread.activeCount());
+//        System.out.println(Thread.currentThread().getName());
+//        Thread.currentThread().setPriority(10);
+
+//        System.out.println(Thread.currentThread().getPriority());
+
+//        System.out.println(Thread.currentThread().isAlive());
+//        for(int i = 0; i <= 3; i++){
+//            System.out.println(i);
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//        }
+
+        MyThread thread2 = new MyThread();
+        thread2.start();
+        System.out.println(thread2.isAlive());
+        thread2.setName("2nd Thread");
+        System.out.println(thread2.getName());
+    }
+}
