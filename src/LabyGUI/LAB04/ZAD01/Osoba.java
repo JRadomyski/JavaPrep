385264a1998795bbf8ca1375f06caa3d10a3ddef
jaayrad <zakubrad@gmail.com>
2023-03-24 18:43:33 +0100

    Cars Collection Exercise

---
+++ src/LabyGUI/LAB04/ZAD01/Osoba.java
@@ -0,0 +1,16 @@
+package LabyGUI.LAB04.ZAD01;
+
+public class Osoba {
+    private String imie;
+    private String naziwsko;
+
+    public Osoba(String imie, String naziwsko) {
+        this.imie = imie;
+        this.naziwsko = naziwsko;
+    }
+
+    @Override
+    public String toString() {
+        return imie + " " + naziwsko;
+    }
+}
