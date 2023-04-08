b52beaf8ab5f9237c9f84c5df816fdd7ed472ec6
Jaayrad <zakubrad@gmail.com>
2023-04-08 22:14:39 +0200

    Bank Account Project

---
+++ src/BankAccount/Account.java
@@ -0,0 +1,22 @@
+package BankAccount;
+
+public class Account {
+    public Account() {
+    }
+    public boolean reduceBalance(long cash){
+        return false;
+    }
+
+    public boolean increaseBalance(long cash){
+        return false;
+    }
+
+    public long getAccountNumber(){
+        return 0;
+    }
+
+    public long getAccountBalance(){
+        return 0;
+    }
+
+}
---
+++ src/BankAccount/Bank.java
@@ -0,0 +1,30 @@
+package BankAccount;
+
+public class Bank {
+    public Bank() {
+    }
+
+    public long createAccount() {
+        return 0;
+    }
+
+    public long withdraw(long accountNumber, long cash) {
+        return 0;
+    }
+
+    public long deposit(long accountNumber, long cash) {
+        return 0;
+    }
+
+    public boolean transfer(long sourceNumber, long destinationNumber, long cash) {
+        return true;
+    }
+
+    public long accountBalance(long accountNumber) {
+        return 0;
+    }
+
+    public long bankBalance() {
+        return 0;
+    }
+}
