c7771326394f03fa93fa40ea726559e430205f86
Jaayrad <zakubrad@gmail.com>
2023-04-08 23:08:18 +0200

    Account class tests

--- src/BankAccount/Account.java
+++ src/BankAccount/Account.java
@@ -33,4 +33,14 @@ public class Account {
         return balance;
     }
 
+    public static void main(String[] args) {
+        Account accountMike = new Account();
+        Account accountJoe = new Account();
+
+        accountMike.increaseBalance(10000);
+        accountJoe.reduceBalance(500);
+        System.out.println(accountMike.getAccountBalance());
+        System.out.println(accountJoe.getAccountBalance());
+    }
+
 }
