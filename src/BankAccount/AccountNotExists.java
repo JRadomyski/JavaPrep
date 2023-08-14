25e1f56c9a9266476119a1da6de5d0663b482324
Jakub Radomyski <zakubrad@gmail.com>
2023-08-14 21:17:39 +0200

    Flushing

--- src/BankAccount/AccountNotExists.java
+++
@@ -1,4 +0,0 @@
-package BankAccount;
-
-class AccountNotExists extends Exception {
-}
--- src/BankAccount/Main.java
+++
@@ -1,7 +0,0 @@
-package BankAccount;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
