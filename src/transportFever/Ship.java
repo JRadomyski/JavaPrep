0f18d6a6d92ce06648bab58ab464fc5307e5c128
Jakub Radomyski <zakubrad@gmail.com>
2023-08-14 21:14:57 +0200

    Opt

--- src/transportFever/Ship.java
+++ src/transportFever/Ship.java
@@ -1,8 +1,15 @@
 package transportFever;
+
+import java.nio.file.*;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 
 public class Ship {
-    private ArrayList<Container> containers = new ArrayList<>();
+    private final ArrayList<Container> containers = new ArrayList<>();
 
     public void loadContainer(Container container) {
         containers.add(container);
@@ -18,5 +25,20 @@ public class Ship {
             totalWeight += container.getWeight();
         }
         return totalWeight;
+    }
+
+    public void arrangeContainers() {
+        containers.sort(Comparator.comparingInt(Container::getWeight));
+    }
+
+    public void saveManifestToFile(String filePath) throws IOException {
+        List<String> lines = new ArrayList<>();
+
+        for(Container container : containers) {
+            String line = container.getId() + "\t" + container.getWeight() + "\t" + container.getType();
+            lines.add(line);
+        }
+
+        Files.write(Paths.get(filePath), lines, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
     }
 }
