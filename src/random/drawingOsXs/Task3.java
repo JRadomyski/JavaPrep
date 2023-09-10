819ab047e2f07cf9fbfc536f274e420242c8d590
Jakub Radomyski <zakubrad@gmail.com>
2023-09-10 22:12:19 +0200

    Random Package

---
+++ src/random/drawingOsXs/Task3.java
@@ -0,0 +1,20 @@
+package random.drawingOsXs;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int n = 4;
+        int size = n * 2 - 1;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                char mark;
+                if (i % 2 == 0 && j % 2 == 0)
+                    mark = 'X';
+                else
+                    mark = '0';
+                System.out.print(mark + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/random/drawingOsXs/Task5.java
@@ -0,0 +1,20 @@
+package random.drawingOsXs;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int n = 4;
+        int size = n * 2 - 1;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                char mark;
+                if (i == 0 || i == size - 1 || i==j || i+j==size-1)
+                    mark = 'X';
+                else
+                    mark = '0';
+                System.out.print(mark + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
