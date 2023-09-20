eedfe3adf6aa620ba6b541331c36d427f700cb10
Jakub Radomyski <zakubrad@gmail.com>
2023-09-20 22:15:26 +0200

    exercises

---
+++ src/Basics/ex1.java
@@ -0,0 +1,47 @@
+package Basics;
+
+/*
+Stwórz program, w którym:
+
+Zapytasz użytkownika, ile imion chce podać.
+Pobierzesz te imiona i zapiszesz je do ArrayListy.
+Wyświetlisz informację, ile jest na liście mężczyzn,
+a ile kobiet, przyjmując że imiona kobiece kończą się na literę “a”.
+ */
+
+
+import codeReading.EGZ;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        int manNum = 0;
+        int femNum = 0;
+
+        ArrayList<String> names = new ArrayList<>();
+        int numOfNames;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ile imion chcesz podać?");
+        numOfNames = scanner.nextInt();
+        scanner.nextLine();
+
+        for(int i = 0; i < numOfNames; i++) {
+            System.out.println("Podaj imie nr. " +(i + 1) + ": ");
+            String name = scanner.nextLine();
+            names.add(name);
+        }
+
+        for(int i = 0; i < names.size(); i++) {
+            if(names.get(i).charAt(names.get(i).length() - 1) == 'a') {
+                femNum++;
+            } else {
+                manNum++;
+            }
+        }
+        System.out.println("Jest tutaj tyle kobiet: " + femNum);
+        System.out.println("Jest tutaj tyle mezczyzn: " + manNum);
+    }
+}
---
+++ src/Basics/ex2.java
@@ -0,0 +1,19 @@
+package Basics;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ex2 {
+    public static void main(String[] args) {
+        String text;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadz zdanie: ");
+        text = scanner.nextLine();
+
+        HashMap<Integer, Character> counter = new HashMap<>();
+
+
+
+
+    }
+}
