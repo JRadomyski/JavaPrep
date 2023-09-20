f99a62c962c66de2f3f1afb8d5ddb47a894830e9
Jakub Radomyski <zakubrad@gmail.com>
2023-09-20 22:15:16 +0200

    Random classes

---
+++ src/Basics/Random.java
@@ -0,0 +1,14 @@
+package Basics;
+
+import java.util.ArrayList;
+
+public class Random {
+    public static void main(String[] args) {
+        ArrayList<String> tab = new ArrayList<>();
+        tab.add("chuj");
+        for (String elem : tab){
+            System.out.println(elem);
+        }
+
+    }
+}
---
+++ src/Basics/test.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class test {
+    public static void main(String[] args) {
+        String name = "Ania";
+
+
+        System.out.println(name.charAt(name.length() - 1));
+
+    }
+}
