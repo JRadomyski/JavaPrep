e5ccc6a58bf91e9502cd5221b5545bf9fd980b14
Jakub Radomyski <zakubrad@gmail.com>
2023-10-09 14:35:19 +0200

    Adding another exercise

--- src/Basics/exercises/ex2.java
+++ src/Basics/exercises/ex2.java
@@ -1,14 +1,25 @@
 package Basics.exercises;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ex2 {
     public static void main(String[] args) {
-        String text;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Wprowadz zdanie: ");
-        text = scanner.nextLine();
-        HashMap<Integer, Character> counter = new HashMap<>();
+        System.out.println("Please enter a sentence:");
+        String sentence = scanner.nextLine();
+        Map<Character, Integer> letterMap = new HashMap<>();
+
+        for (char letter : sentence.toCharArray()) {
+            if (Character.isLetter(letter)) {
+                letter = Character.toLowerCase(letter);
+                letterMap.put(letter, letterMap.getOrDefault(letter, 0) + 1);
+            }
+        }
+        for (Map.Entry<Character, Integer> entry : letterMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
+
